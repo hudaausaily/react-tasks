b834a3be147f6ecf3eea4e0a1817c4475effd416
Huda Al-Ausaily <hudaausaily@gmail.com>
2023-02-13 16:06:20 +0300

    task

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './comp/login';
+import Header from './comp/header';
+import './App.css';
+
+function App(){
+
+
+  return(
+  <div>
+  <Header/>
+  <Login/>
+  </div>
+
+);
+
+
+}
+
+
+export default App
